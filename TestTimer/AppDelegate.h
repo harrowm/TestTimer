12e00a0c0cf8fccc4716fbfe3e5dca1987faa3c8
Malcolm <malcolm@Malcolms-Mac-Pro.local>
2014-12-31 08:11:42 +0800

    Initial Commit

---
+++ TestTimer/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  TestTimer
+//
+//  Created by Malcolm on 31/12/14.
+//  Copyright (c) 2014 Malcolm Harrow. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
---
+++ TestTimer/AppDelegate.m
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.m
+//  TestTimer
+//
+//  Created by Malcolm on 31/12/14.
+//  Copyright (c) 2014 Malcolm Harrow. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@interface AppDelegate ()
+
+@property (weak) IBOutlet NSWindow *window;
+@end
+
+@implementation AppDelegate
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification {
+    // Insert code here to initialize your application
+}
+
+- (void)applicationWillTerminate:(NSNotification *)aNotification {
+    // Insert code here to tear down your application
+}
+
+@end
